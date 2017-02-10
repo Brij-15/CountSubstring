5c4c5f142a68930bc6e87d8b775dc758dd548742
Brij Patel <brij.15@live.ca>
2017-02-10 13:17:14 -0500

    “done ArrayList”

--- CountSubstrings.java
+++ CountSubstrings.java
@@ -1,12 +1,12 @@
+import javax.swing.*;
 import java.util.*;
 import java.io.*;
 public class CountSubstrings {
 	public static int index = 0;
 	public static void main(String [] args) throws IOException {
-	File text1 =  new File("Les Miserables.txt");
+	File text1 =  new File("Les Miserables");
 	File text2 = new File("A Tale of Two Cities.txt");
 	String name = null;
-	BufferedReader reader = new BufferedReader (new FileReader(name));
   String pattern = "Javert";
   String line = "";
   LinkedList<Character> list = new LinkedList<Character>();
@@ -15,20 +15,23 @@ public class CountSubstrings {
 	ArrayList<Character> patternArray = new ArrayList<Character>();
   int occuranceL = 0;
   int occuranceA = 0;
-  double listTimeE = 0, arrTimeE = 0;
-  double listTimeS = 0, arrTimeS = 0;
+  double listTimeE = 0;
+  double arrTimeE = 0;
+  double listTimeS = 0;
+  double arrTimeS = 0;
 
 	for(int i=0;i < pattern.length();i++){ //put pattern into arraylist and linkedlist
 		patternList.add(pattern.substring(i).charAt(0)); //add char at position i index 0 into list
 		patternArray.add(pattern.substring(i).charAt(0));
 	}
-  BufferedReader reader = new BufferedReader (text1);
-  line = reader.getLine();
+  FileReader fileRead = new FileReader (text1);
+  BufferedReader reader = new BufferedReader(fileRead);
+  line = reader.readLine();
   listTimeS = System.currentTimeMillis();
 
     while(line != null){ //adds line to ArrayList
       for(char c : line.toCharArray()){
-        Alist.add(c);
+        AList.add(c);
       }
 			occuranceA = occuranceA + patternFinder(AList,patternList, pattern, 0);
 			AList = new ArrayList<Character>();
@@ -36,26 +39,26 @@ public class CountSubstrings {
     }
 		reader.close();
 		arrTimeE = System.currentTimeMillis();
-			System.out.println("Using ArrayList :"+occuranceA+"matches were found, derived in"+(arrtimeE - arrtimeS)+"milliseconds");
+			System.out.println("Using ArrayList: "+occuranceA+" matches were found, derived in "+(arrTimeE - arrTimeS)+" milliseconds");
 	} //done arrayList checking
 
 
 
-public static int patternFinder(List<Character> text, List<Character> pattern, String pattern, int position){
+public static int patternFinder(List<Character> text, List<Character> pattern, String pattern1, int position){
 	int count = 0;
 	index = findBrute(text, pattern, position);
 			while(index != -1){ //find brute returns -1 is match is not found so you dont need to go into the loop
-				for(int i = index; i < index + pattern.length(); i++){
+				for(int i = index; i < index + pattern1.length(); i++){
 					if(!(text.get(i).equals(pattern.get(i-index)))){
 						count = count + 0;
 					}
 					else {
-						if(i == index + pattern.length() - 1){
+						if(i == index + pattern1.length() - 1){
 							count = count + 1;
 						}
 					}//end of else
 				}//end of for
-				index = findBrute(text, pattern, index + pattern.length());
+				index = findBrute(text, pattern, index + pattern1.length());
 			}//end of while
 			return count;
 }//end findPattern
